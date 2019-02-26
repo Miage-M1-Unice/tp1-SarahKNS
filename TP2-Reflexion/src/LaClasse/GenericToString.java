5639c51cbe35a7f4382f927733c4ae594b52a5f5
SarahKNS <sarah.nassik@etu.unice.fr>
2019-02-26 09:11:16 +0100

    Correction

--- TP2-Reflexion/src/LaClasse/GenericToString.java
+++ TP2-Reflexion/src/LaClasse/GenericToString.java
@@ -1,25 +1,24 @@
 package LaClasse;
+
 import java.awt.Point;
 import java.awt.Polygon;
 import java.lang.reflect.Field;
 
 public class GenericToString {
 
-	public  String toString(Object obj,  int profondeur) {
-		String str=" ";
+	public String toString(Object obj, int profondeur) {
+		String str = " ";
 		Class cl = obj.getClass();
 		str += cl.getName();
 		str += "[ ";
-		Field[] fields =cl.getFields();
 
-
-		for(Field f:fields) {
-			//toString(f, profondeur -1);
+		Field[] fields = cl.getDeclaredFields();
+		for (Field f : fields) {
+			f.setAccessible(true);
 			str += f.getName();
 			str += " = ";
 			try {
-				
-				str+= f.get(obj);
+				str += f.get(obj);
 			} catch (IllegalArgumentException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -28,24 +27,21 @@ public class GenericToString {
 				e.printStackTrace();
 			}
 		}
-		
-		str+=
-		
-		str += "=";
 		str += " ;";
 		str += "]";
-		return str;
 
+		str += cl.getInterfaces();
+		return str;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GenericToString g = new GenericToString();
-		System.out.println(g.toString(new Point(12,24), 2));  
+		System.out.println(g.toString(new Point(12, 24), 2));
 
-		Polygon pol = new Polygon(new int[]{10, 20, 30}, new int[]{20,30, 40}, 3);  
-		pol.getBounds();  
-		System.out.println(new GenericToString().toString());  
+		Polygon pol = new Polygon(new int[] { 10, 20, 30 }, new int[] { 20, 30, 40 }, 3);
+		pol.getBounds();
+		System.out.println(new GenericToString().toString());
 	}
 
 }
